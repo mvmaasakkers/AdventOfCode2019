ba45f919a83fb0ca9f29e764cc607268884161cc
Martijn van Maasakkers <martijn@vanmaasakkers.net>
2019-12-02 22:20:01 +0100

    Compacted opcode handler into function

--- day02/day02.py
+++ day02/day02.py
@@ -10,24 +10,20 @@ part2 = 0
 
 answerP2 = 19690720
 
+def runOpcode(loc, opcode, param1Pos, param2Pos):
+    if opcode == 1:
+        return loc[loc[param1Pos]] + loc[loc[param2Pos]]
+
+    if opcode == 2:
+        return loc[loc[param1Pos]] * loc[loc[param2Pos]]
+
 pos = 0
 locations = list.copy(origLocations)
 locations[1] = 12
 locations[2] = 2
 while True:
     opcode = locations[pos]
-    if opcode == 1:
-        # Add
-        param1 = locations[locations[pos+1]]
-        param2 = locations[locations[pos+2]]
-        locations[locations[pos+3]] = param1 + param2
-
-    if opcode == 2:
-        # Multiply
-        param1 = locations[locations[pos+1]]
-        param2 = locations[locations[pos+2]]
-        locations[locations[pos+3]] = param1 * param2
-
+    locations[locations[pos+3]] = runOpcode(locations, opcode, pos+1, pos+2)
     if opcode == 99:
         # End
         part1 = locations[0]
@@ -47,18 +43,8 @@ for i in range(1,99):
         pos = 0
         while True:
             opcode = locations[pos]
-            if opcode == 1:
-                # Add
-                param1 = locations[locations[pos+1]]
-                param2 = locations[locations[pos+2]]
-                locations[locations[pos+3]] = param1 + param2
-
-            if opcode == 2:
-                # Multiply
-                param1 = locations[locations[pos+1]]
-                param2 = locations[locations[pos+2]]
-                locations[locations[pos+3]] = param1 * param2
-
+            locations[locations[pos+3]] = runOpcode(locations, opcode, pos+1, pos+2)
+            
             if opcode == 99:
                 # End
                 res = locations[0]
