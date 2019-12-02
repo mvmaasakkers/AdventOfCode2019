3f2162ba05b8fa5f7fad935765f26c49efc32f51
Martijn van Maasakkers <martijn@vanmaasakkers>
2019-12-02 05:09:45 +0100

    Added solution for day01

---
+++ day01/day01.py
@@ -0,0 +1,27 @@
+from itertools import cycle
+import math
+
+modules = [];
+with open('day01/input.txt', 'r') as f:
+    for line in f:
+        if line != '':
+            modules.append(int(line));
+
+part1 = 0;
+part2 = 0;
+
+for module in modules:
+    fuel = math.floor(module / 3) - 2
+    part1 = part1 + fuel
+    mass = fuel
+    while True:
+        extraFuel = math.floor(mass / 3) - 2
+        if extraFuel > 0:
+            fuel = fuel + extraFuel
+        if extraFuel <= 0:
+            break
+        mass = extraFuel
+    part2 = part2 + fuel
+
+print("Part 1:", part1);
+print("Part 2:", part2);
