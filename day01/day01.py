e716af55f0355dc281689ebb58688897223d5931
Martijn van Maasakkers <martijn@vanmaasakkers.net>
2019-12-02 22:33:30 +0100

    Compacted day02 further

--- day01/day01.py
+++ day01/day01.py
@@ -7,21 +7,21 @@ with open('day01/input.txt', 'r') as f:
         if line != '':
             modules.append(int(line));
 
-part1 = 0;
-part2 = 0;
+part1 = 0
+part2 = 0
 
 for module in modules:
     fuel = math.floor(module / 3) - 2
-    part1 = part1 + fuel
+    part1 += fuel
     mass = fuel
     while True:
         extraFuel = math.floor(mass / 3) - 2
         if extraFuel > 0:
-            fuel = fuel + extraFuel
+            fuel += extraFuel
         if extraFuel <= 0:
             break
         mass = extraFuel
-    part2 = part2 + fuel
+    part2 += fuel
 
 print("Part 1:", part1);
 print("Part 2:", part2);
--- day02/day02.py
+++ day02/day02.py
@@ -1,59 +1,35 @@
-
-origLocations = []
-with open('day02/input.txt', 'r') as f:
-    for line in f:
-        if line != '':
-            origLocations = list(map(int, line.split(",")))
-
-part1 = 0
-part2 = 0
-
-answerP2 = 19690720
-
-def runOpcode(loc, opcode, param1Pos, param2Pos):
-    if opcode == 1:
-        return loc[loc[param1Pos]] + loc[loc[param2Pos]]
-
-    if opcode == 2:
-        return loc[loc[param1Pos]] * loc[loc[param2Pos]]
-
-pos = 0
-locations = list.copy(origLocations)
-locations[1] = 12
-locations[2] = 2
-while True:
-    opcode = locations[pos]
-    locations[locations[pos+3]] = runOpcode(locations, opcode, pos+1, pos+2)
-    if opcode == 99:
-        # End
-        part1 = locations[0]
-        break
-
-    pos = pos + 4
+def get_data(file):
+    with open(file, 'r') as f:
+        data = list(map(int, f.read().split(',')))
+    return data
 
 
-for i in range(1,99):
+origLocations = get_data('day02/input.txt')
+
+def part_one(locs, noun, verb):
+    loc = list.copy(locs)
+    loc[1] = noun
+    loc[2] = verb
+    pos = 0
+    while True:
+        opcode = loc[pos]
+        if opcode == 1:
+            loc[loc[pos+3]] = loc[loc[pos+1]] + loc[loc[pos+2]]
+
+        if opcode == 2:
+            loc[loc[pos+3]] = loc[loc[pos+1]] * loc[loc[pos+2]]
+        if opcode == 99:
+            # End
+            return loc[0]
+            break
+
+        pos = pos + 4
+
+def part_two(origLocations, output):
+  for i in range(1,99):
     for j in range(1, 99):
-        locations = list.copy(origLocations)
-        # print(locations)
-        # exit
-        locations[1] = i
-        locations[2] = j
-        res = 0
-        pos = 0
-        while True:
-            opcode = locations[pos]
-            locations[locations[pos+3]] = runOpcode(locations, opcode, pos+1, pos+2)
-            
-            if opcode == 99:
-                # End
-                res = locations[0]
-                if res == answerP2:
-                    part2 = (100 * i + j)
-                break
+        if part_one(origLocations, i, j) == output:
+            return (100 * i) + j
 
-            pos = pos + 4
-
-
-print("Part 1:", part1);
-print("Part 2:", part2);
+print("Part 1:", part_one(origLocations, 12, 2));
+print("Part 2:", part_two(origLocations, 19690720));
