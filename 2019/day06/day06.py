9b74cfefd19879c5cb073f0f7f5507a7de802142
Martijn van Maasakkers <martijn@vanmaasakkers.net>
2019-12-06 20:17:32 +0100

    Combined traverse and traverseTo in 1 function, also use unfolding returns instead of an accumulator

--- 2019/day06/day06.py
+++ 2019/day06/day06.py
@@ -3,29 +3,20 @@ objects = {l[1]: [] for l in data}
 for line in data:
     objects[line[1]].append(line[0])
 
-def traverse(obj, i):
-    i.append(obj)
-    if obj in objects:
-        for o in objects[obj]:
-            if o in objects:
-                return traverse(o, i)
-    return i
-
-def traverseTo(obj, to, i):
-    i.append(obj)
+def traverse(obj, to):
     if obj in objects:
         if obj != to:
             for o in objects[obj]:
                 if o in objects:
-                    return traverseTo(o, to, i)
-    return i
+                    return [obj] + traverse(o, to)
+    return [obj]
 
-print("Part 1:", sum([len(traverse(o, [])) for o in objects]))
+print("Part 1:", sum([len(traverse(o, 'COM')) for o in objects]))
 
-youToCOM = traverse('YOU', [])
-sanToCOM = traverse('SAN', [])
+youToCOM = traverse('YOU', 'COM')
+sanToCOM = traverse('SAN', 'COM')
 
 base = [planet for planet in youToCOM if planet in sanToCOM][0]
 
 # The 2 times - 2 are for removing the start and end object from the list
-print("Part 2:", len(traverseTo('YOU', base, [])) - 2 + len(traverseTo('SAN', base, [])) - 2)
+print("Part 2:", len(traverse('YOU', base)) - 2 + len(traverse('SAN', base)) - 2)
