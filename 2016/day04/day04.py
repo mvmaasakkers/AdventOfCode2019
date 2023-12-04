bf18acb55cf85ac256394adcb727e1cf4d9e1646
Martijn van Maasakkers <martijn@vanmaasakkers.net>
2023-12-04 08:51:27 +0100

    Cleanup

--- 2016/day04/day04.py
+++ 2016/day04/day04.py
@@ -1,15 +1,17 @@
 def part1(file_path):
     with open(file_path, 'r') as file:
-        input_data = list(file.read().splitlines())
+        input_data = [line for line in file.read().splitlines()]
+        answer = 0
 
-        return 0
+        return answer
 
 
 def part2(file_path):
     with open(file_path, 'r') as file:
-        input_data = list(file.read().splitlines())
+        input_data = [line for line in file.read().splitlines()]
+        answer = 0
 
-        return 0
+        return answer
 
 
 print("Part 1: ", part1('input_test.txt'))
