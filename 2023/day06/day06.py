645d5ef63ec3c3b26162d0403ce8fcf9d0ae19d3
Martijn van Maasakkers <martijn@vanmaasakkers.net>
2023-12-06 09:12:16 +0100

    Added 2023/day06

--- 2023/day06/day06.py
+++ 2023/day06/day06.py
@@ -1,24 +1,31 @@
+import math
+import re
+
+
 def part1(file_path):
     with open(file_path, 'r') as file:
         input_data = [line for line in file.read().splitlines()]
-        answer = 0
 
-        for line in input_data:
-            print(line)
+        times = [int(n) for n in re.split(r'\s+', input_data[0][len("Time: "):].strip())]
+        distances = [int(n) for n in re.split(r'\s+', input_data[1][len("Distance: "):].strip())]
+        races = [{'time': times[i], 'distance': distances[i]} for i in range(len(times))]
 
-        return answer
+        possibilities = [
+            sum([1 for time in range(0, race['time'] + 1) if time * (race['time'] - time) > race['distance']]) for race
+            in races]
+
+        return math.prod(possibilities)
 
 
 def part2(file_path):
     with open(file_path, 'r') as file:
         input_data = [line for line in file.read().splitlines()]
-        answer = 0
 
-        for line in input_data:
-            print(line)
+        total_time = int(''.join([n for n in re.split(r'\s+', input_data[0][len("Time: "):].strip())]))
+        total_distance = int(''.join([n for n in re.split(r'\s+', input_data[1][len("Distance: "):].strip())]))
 
-        return answer
+        return sum([1 for time in range(0, total_time + 1) if time * (total_time - time) > total_distance])
 
 
-print("Part 1: ", part1('input_test.txt'))
-print("Part 2: ", part2('input_test.txt'))
+print("Part 1: ", part1('input.txt'))
+print("Part 2: ", part2('input.txt'))
